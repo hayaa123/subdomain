1ec830468a0ff6da62b6a3b9d646d287023ee97c
Abrar Nedal Alzubidi <a.alzubidi@ltuc.com>
2022-03-21 08:21:50 +0300

    fix style

--- flexPage/models.py
+++ flexPage/models.py
@@ -1,14 +1,23 @@
 from re import template
 from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel ,StreamFieldPanel
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel ,StreamFieldPanel, MultiFieldPanel
+from wagtail.snippets.models import register_snippet
+from wagtail.images.edit_handlers import ImageChooserPanel 
+from modelcluster.fields import ParentalKey 
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+
+
 from wagtail.core.fields import StreamField
 from streams import blocks, website1bocks
 # Create your models here.
 
 class FlexPage(Page):
     template ='flexPage/flex_page.html'
+    parent_page_types = ['home.HomePage']
+
     content = StreamField(
         [
             ("title_and_text", blocks.TitleAndTextBlock()),
@@ -26,6 +35,8 @@ class FlexPage(Page):
 
 class FlexPageWebsite1(Page):
     template ='flexPage/flex_page.html'
+    parent_page_types = ['home.HomePageWebsite1']
+
     content = StreamField(
         [
             ("title_and_text", website1bocks.TitleAndTextBlock()),
@@ -40,3 +51,4 @@ class FlexPageWebsite1(Page):
     content_panels = Page.content_panels +[
         StreamFieldPanel("content")
        ]
+
--- home/models.py
+++ home/models.py
@@ -1,12 +1,70 @@
 from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel ,StreamFieldPanel
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import InlinePanel ,StreamFieldPanel, FieldPanel, MultiFieldPanel
 from streams import blocks ,website1bocks
 from wagtail.core.fields import StreamField
+from wagtail.snippets.models import register_snippet
+from wagtail.images.edit_handlers import ImageChooserPanel 
+from modelcluster.fields import ParentalKey 
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+
+class TeacherOrderable(Orderable):
+    """
+    allow us to select one or more teacher
+    """
+    page = ParentalKey("home.HomePage",related_name="teachers")
+    teacher = models.ForeignKey(
+        "home.Teacher",
+        on_delete = models.CASCADE,
+    )
+
+    panels = [
+        SnippetChooserPanel("teacher")
+    ]
+
+class Teacher(models.Model):
+    """
+    teacher model for snippets
+    """
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True, null=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="+"
+    )
+
+    panels = [MultiFieldPanel(
+        [
+            FieldPanel("name"),
+            ImageChooserPanel("image")
+        ],
+        heading="name and Image"
+    ),
+        MultiFieldPanel([
+            FieldPanel("website")
+        ],
+        heading="link"
+        )
+    ]
+
+    def __str__(self) -> str:
+        return self.name
+    class Meta :
+        verbose_name = "Teacher"
+        verbose_name_plural = "Teachers"
+
+register_snippet(Teacher)
+
 
 class HomePage(Page):
     template = "home/home_page.html"
+    max_count = 1
+    subpage_types = ['flexPage.FlexPage']
+    parent_page_types = ['wagtailcore.Page']
     content = StreamField(
         [
             ("title_and_text", blocks.TitleAndTextBlock()),
@@ -20,7 +78,8 @@ class HomePage(Page):
 
 
     content_panels = Page.content_panels +[
-        StreamFieldPanel("content")
+        StreamFieldPanel("content"),
+        InlinePanel("teachers",label="teacher",min_num=1,max_num=4)
        ]
 
 
@@ -33,6 +92,9 @@ class HomePage(Page):
 # all have the same template ???!! 
 class HomePageWebsite1(Page):
     template = "home/home_page_website1.html"
+    max_count = 1
+    subpage_types = ['flexPage.FlexPageWebsite1']
+    parent_page_types = ['wagtailcore.Page']
 
     content = StreamField(
         [
@@ -51,3 +113,5 @@ class HomePageWebsite1(Page):
 
 class HomePageWebsite2(HomePage):
     template = "home/home_page_website2.html"
+    max_count = 1
+
